3df460d39e48456a9aefcf0651f03203ea82e6dd
Dan Bock <danbock@gmail.com>
2021-07-09 07:23:29 -0400

    Add responsive grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
-import './App.css'
+import './App.scss'
 
 function App() {
   return (
     <div className='App'>
-      {Array.from(Array(44).keys()).map(key => (
-        <img
-          key={key}
-          alt={`${key}`}
-          src={`img/915 Urban${('0' + (key + 1)).slice(-2)}.jpg`}
-        />
-      ))}
+      <h1>For Rent: 915 Urban Ave.</h1>
+
+      <div className='images'>
+        {Array.from(Array(44).keys()).map(key => (
+          <img
+            key={key}
+            alt={`${key}`}
+            src={`img/915 Urban${('0' + (key + 1)).slice(-2)}.jpg`}
+          />
+        ))}
+      </div>
     </div>
   )
 }
