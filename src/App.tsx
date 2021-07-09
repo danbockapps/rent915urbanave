6329820e42f14cd3d8fc1dc44fe0659406fa3748
Dan Bock <danbock@gmail.com>
2021-07-09 09:54:26 -0400

    Add prettier settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,16 @@
-import React from 'react'
+import React, { FC } from 'react'
 import './App.scss'
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>For Rent: 915 Urban Ave.</h1>
+const App: FC = () => (
+  <div className='App'>
+    <h1>For Rent: 915 Urban Ave.</h1>
 
-      <div className='images'>
-        {Array.from(Array(44).keys()).map(key => (
-          <img
-            key={key}
-            alt={`${key}`}
-            src={`img/915 Urban${('0' + (key + 1)).slice(-2)}.jpg`}
-          />
-        ))}
-      </div>
+    <div className='images'>
+      {Array.from(Array(44).keys()).map(key => (
+        <img key={key} alt={`${key}`} src={`img/915 Urban${('0' + (key + 1)).slice(-2)}.jpg`} />
+      ))}
     </div>
-  )
-}
+  </div>
+)
 
 export default App
